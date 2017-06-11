fa3bc7f1e0601c9d99d2811bc12cd3421baf784e
Josh Arnold <josharnold997@gmail.com>
2017-06-11 20:11:10 +1200

    Yas

--- python/vcm.py
+++
@@ -1,6 +0,0 @@
-# Trade Me view count maxer 
-
-import urllib2
-for lp in range(7000):
-	urllib2.urlopen("http://www.trademe.co.nz/computers/laptops/laptops/apple/auction-1307439363.htm").read()
-	print(lp)
